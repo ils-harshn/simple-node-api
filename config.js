12490a1424e02e45bd858e179f4e0c2325180c9f
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-09-18 13:32:34 +0530

    process.env.SECRET_VERSION_NAME

--- config.js
+++ config.js
@@ -7,12 +7,14 @@ let CONFIG = process.env;
 
 const load_config = async () => {
   try {
+    console.log("process.env.SECRET_VERSION_NAME:", process.env.SECRET_VERSION_NAME);
+    
     const [version] = await client.accessSecretVersion({
       name: process.env.SECRET_VERSION_NAME,
     });
 
-    const payload = version.payload.data.toString("utf8");
     console.log("TAKING GCP SECRET RESOURCE", version.payload.data);
+    const payload = version.payload.data.toString("utf8");
     CONFIG = JSON.parse(payload);
   } catch {
     console.log("TAKING LOCAL RESOURCE")
